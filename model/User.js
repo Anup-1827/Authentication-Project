5794536c9b07fa07141436545fbfff133eeddb5c
Anup-1827 <anupalone.off@gmail.com>
2024-03-31 21:58:57 +0530

    New Auth has been added

--- model/User.js
+++ model/User.js
@@ -21,6 +21,12 @@ const UserSchema = new Schema({
     password:{
         type: String,
         required : true
+    },
+    role:{
+        type: String,
+        enum: ["admin", "user"],
+        default: "user",
+        required: true
     }
 },{
     timestamps: true
